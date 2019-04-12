d7212161e719e2e72201a21361d7698d1cc95875
Kevin Lind <lind@adobe.com>
2019-04-12 12:52:27 -0700

    initial add of ACPSkeletonExtension for iOS

---
+++ iOS/ACPSkeletonExtension/TestApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestApp
+//
+//  Created by lind on 4/12/19.
+//  Copyright © 2019 Adobe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ iOS/ACPSkeletonExtension/TestApp/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  TestApp
+//
+//  Created by lind on 4/12/19.
+//  Copyright © 2019 Adobe. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
